fed8d859a706a53ee07130c8cfc0b2f810c0e3ec
binattimattia <binattimattia@gmail.com>
2026-04-24 10:27:47 +0200

    refactor: add input validation, error handling, and rename compute function

--- rules.py
+++ rules.py
@@ -1,12 +1,22 @@
 def is_even(number: int) -> bool:
-    return number % 2 == 0
+    try:
+        return number % 2 == 0
+    except (TypeError, ValueError) as e:
+        raise ValueError(f"The character is not a number")
 
 def is_vowel(letter: str) -> bool:
-    return letter.lower() in ["a", "e", "i", "o", "u"]
+    try:
+        return letter.lower() in ["a", "e", "i", "o", "u"]
+    except (TypeError, ValueError) as e:
+        raise ValueError(f"The character is not a letter")
 
-def compute_expected_number(position: str, letter: str, number: int) -> bool:
-    if position.lower() == "top":
-        return is_even(number)
-    else:
-        return is_vowel(letter)
-        
+def compute_expected_answer(position: str, letter: str, number: int) -> bool:
+    try:
+        if position.lower() == "top":
+            return is_even(number)
+        elif position.lower() == "bottom":
+            return is_vowel(letter)
+        else:
+            raise ValueError(f"Invalid position: {position}")
+    except (AttributeError, TypeError) as e:
+        raise ValueError(f"The characters are incompatible: {e}")
